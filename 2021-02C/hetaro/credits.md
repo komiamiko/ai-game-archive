f29068795bf771dabce87f4d0ce2677997fa74ce
Komi Amiko <49998816+komiamiko@users.noreply.github.com>
2021-06-02 00:03:23 -0400

    Revert "Tournament: Ricochet"

--- 2021-02C/hetaro/credits.md
+++
@@ -1,3 +0,0 @@
-All scripts: [\@komiamiko](https://github.com/komiamiko/)
-
-Compute for training: [\@hydargos123](https://github.com/hydargos123)
--- 2021-02C/setup.sh
+++
@@ -1,6 +0,0 @@
-python3 -m pip install gym numpy torch stable-baselines3
-cd tj
-python3 setup.py install
-cd ../hetaro
-python3 setup.py install
-cd ..
